244d97c6d939df372b8eb22429073de692dd41f1
Neil Byrne <G00343624@gmit.ie>
2021-01-08 03:40:11 +0000

    Web service implemets Decision Tree Regression

--- webService.py
+++ webService.py
@@ -1,22 +1,47 @@
-# flask for web app.
-import flask as fl
-# numpy for numerical work.
+from flask import Flask,redirect,request
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
+import pandas as pd
 
-# Create a new web app.
-app = fl.Flask(__name__)
+# This code is adapted from the Jupyter notebook
+lin_data = pd.read_csv('powerproduction.csv')
 
-# Add root route.
-@app.route("/")
+# Outliers found in Jupyter notebook
+lin_data = lin_data.drop([208, 340, 404, 456, 490, 491, 492, 493, 494, 495, 496, 497, 498, 499])
+
+X = lin_data.iloc[:, 0].values
+y = lin_data.iloc[:, 1].values
+X = X.reshape(-1, 1)
+
+# Decision Tree Regression Training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)  # Training set is 20% of total
+decregressor = DecisionTreeRegressor()  # Perfrom the Regression
+decregressor.fit(X_train, y_train)
+
+def decTree(speed):
+    speed_arr = np.array(speed).reshape(-1, 1)
+    return str(decregressor.predict(speed_arr)[0])
+
+# declare app
+app = Flask(__name__)
+
+# Return home 
+@app.route('/')
 def home():
-  return app.send_static_file('index.html')
+    return redirect("static/index.html")
+speed = 0
 
-# Add uniform route.
-@app.route('/api/uniform')
+# Decision Tree Regression Training
+@app.route("/api/model1", methods = ["GET", "POST"])
 def uniform():
-  return {"value": np.random.uniform()}
+    global speed
+    if request.method == "POST":
+        speed = float(request.json)
+    return {"value": decTree(speed)}
 
-# Add normal route.
-@app.route('/api/normal')
-def normal():
-  return {"value": np.random.normal()}
+# Run
+if __name__ == '__main__':
+    app.run()
